200d8d642834bb480a15b784cac5a64a339da5f0
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-11 17:00:38 +0000

    Learning React.

---
+++ GettingStarted/src/index.js
@@ -0,0 +1,10 @@
+function Person(props) {
+  return (
+    <div className="person">
+      <h1>props.name</h1>
+      <p>Age: props.age</p>
+    </div>
+  );
+}
+
+React.DOM.render(<Person name="Jacob" age="32"/>, document.querySelector('#p1'))
