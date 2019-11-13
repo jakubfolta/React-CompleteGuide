4b4af0b5a67b0014280a72aa67695b756942e5b0
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-13 23:17:41 +0000

    Learn React.

--- BaseFeaturesSyntax/react-complete-guide/src/App.js
+++ BaseFeaturesSyntax/react-complete-guide/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm React app.</h1>
-    //   </div>
-    // );
-    return React.createElement('div', null, React.createElement('h1', null, 'Hi I\'m React appppp!!!'));
+    return (
+      <div className="App">
+        <h1>Hi, I'm React app.</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React appppp!!!'));
   }
 }
 
