9c096bac7cced0aaa62bb7beffaeb1c1ae20bd0b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-14 00:30:05 +0000

    Learn React.

--- BaseFeaturesSyntax/react-complete-guide/src/App.js
+++ BaseFeaturesSyntax/react-complete-guide/src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 32 },
+      { name: 'Dom', age: 23 },
+      { name: 'Bryan', age: 35 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked !');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React app.</h1>
         <p>This is working.</p>
-        <Person name="Max" age="32" />
-        <Person name="Dom" age="23">My hobbies: Racing</Person>
-        <Person name="Bryan" age="43" />
+        <button onClick={this.switchNameHandler}>Switch name!</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'m React appppp!!!'));
--- BaseFeaturesSyntax/react-complete-guide/src/Person/Person.js
+++ BaseFeaturesSyntax/react-complete-guide/src/Person/Person.js
@@ -7,7 +7,6 @@ const person = (props) => {
       <p>{props.children}</p>
     </div>
   )
-
 };
 
 export default person;
