7b3a3237d627f27a01e331abae903ab15e378b2b
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-15 00:55:09 +0000

    Base features - assignment.

--- basefeatures-assignment/src/App.js
+++ basefeatures-assignment/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
-  
+
 
   render() {
     return (
