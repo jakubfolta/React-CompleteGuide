f35f14b4edde30548a9943417b3478cd41297151
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-15 01:47:50 +0000

    Base features - assignment.

--- basefeatures-assignment/src/App.js
+++ basefeatures-assignment/src/App.js
@@ -4,16 +4,18 @@ import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
-
+  state = {
+    userName: 'Noctis'
+  };
 
   render() {
     return (
       <div>
         <h1>Hello, I'm React app !!!</h1>
-        <UserOutput userName="Ignis"/>
-        <UserOutput userName="Gladio"/>
-        <UserOutput userName="Noct"/>
-        <UserOutput userName="Promto"/>
+        <UserOutput userName="Ignis" />
+        <UserOutput userName="Gladio" />
+        <UserOutput userName={this.state.userName} />
+        <UserOutput userName="Promto" />
       </div>
     );
   }
