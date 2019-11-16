bde4a52ea8e2ef24fde809d5dad7914e577a08d8
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-16 01:22:19 +0000

    Continue 'Working with lists and conditionals' section - lists & state.

--- ListsConditionals/lists-conditionals/src/App.js
+++ ListsConditionals/lists-conditionals/src/App.js
@@ -13,18 +13,6 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = newName => {
-    // console.log('Was clicked !');
-    // DON'T DO THIS this.state.persons[0].name = 'Jake';
-    this.setState({
-      persons: [
-        { name: newName, age: 32 },
-        { name: 'Dom', age: 38 },
-        { name: 'Bryan', age: 35 }
-      ]
-    })
-  }
-
   nameChangedHandler = event => {
     this.setState({
       persons: [
@@ -33,6 +21,12 @@ class App extends Component {
         { name: 'Bryan', age: 35 }
       ]
     })
+  }
+
+  deletePersonHandler = personIndex => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () => {
@@ -56,16 +50,12 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
           })}
-          <Person
-            name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Noct')}
-            changed={this.nameChangedHandler} >My hobbies: Racing</Person>
-          <Person
-            name={this.state.persons[2].name} age={this.state.persons[2].age} />
         </div>
       );
     }
