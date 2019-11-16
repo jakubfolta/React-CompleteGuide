31be1f05d5714f398a12046ef39ee16a18027ebd
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-16 02:33:48 +0000

    Continue 'Working with lists and conditionals' section - flexible lists.

--- ListsConditionals/lists-conditionals/src/App.js
+++ ListsConditionals/lists-conditionals/src/App.js
@@ -19,16 +19,15 @@ class App extends Component {
     });
 
     const person = {
-      ...this.state.persons[personIndex].name = event.target.value
+      ...this.state.persons[personIndex]
     };
 
-    this.setState({
-      persons: [
-        { name: 'Max', age: 32 },
-        { name: event.target.value, age: 28 },
-        { name: 'Bryan', age: 35 }
-      ]
-    })
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( { persons: persons} )
   }
 
   deletePersonHandler = personIndex => {
