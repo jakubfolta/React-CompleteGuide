e676210a2a3dd2a1b859ba46e91eaf936b35c835
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-21 23:37:32 +0000

    Solidifying assignment - repeat.

--- lists-conditionals-assignment/src/Validation/Validation.js
+++ lists-conditionals-assignment/src/Validation/Validation.js
@@ -1,4 +1,13 @@
+import React from 'react';
 
+const validation = props => {
+  const message = props.length > 5 ? 'Text length is ok!' : 'Text is too short!!!'; 
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  )
+}
 
 
 
