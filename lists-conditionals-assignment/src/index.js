c1bd7f8feeeb23495ce34030a626e83a540d03c3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-26 23:44:58 +0000

    Complete assignment again - solidifying knowledge.

--- lists-conditionals-assignment/src/index.js
+++ lists-conditionals-assignment/src/index.js
@@ -6,11 +6,3 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
-
-
-import React from 'react';
-import ReactDom from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDom.render(<App />, document.getElementById('root'));
