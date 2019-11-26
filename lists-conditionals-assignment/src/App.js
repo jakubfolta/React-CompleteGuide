7f33cd9b661ec331d1bba305c92feb771b879a3f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-26 23:39:34 +0000

    Complete assignment again - solidifying knowledge.

--- lists-conditionals-assignment/src/App.js
+++ lists-conditionals-assignment/src/App.js
@@ -1,3 +1,8 @@
+import React, {Component} from 'react';
+import './App.css'
+
+
+
 <ol>
   <li>Create an input field in App component with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
   <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
