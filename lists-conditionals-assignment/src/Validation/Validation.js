52b2c068328fd505dbb11a6216e1bd424c7ae6de
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-27 00:19:37 +0000

    Complete assignment again (Task 3) - solidifying knowledge.

--- lists-conditionals-assignment/src/Validation/Validation.js
+++ lists-conditionals-assignment/src/Validation/Validation.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const validation = props => {
+  const message = props.length > 5 ? 'Text length is fine' : 'Text too short';
+
   return (
     <div>
-      {props.length}
+      {message}
     </div>
   )
 };
