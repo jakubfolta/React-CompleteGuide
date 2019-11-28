ba226ec1afbfb7b71e9fe62604a72d1e891ea58d
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-28 00:58:33 +0000

    Complete assignment again (Task 1) - solidifying knowledge.

--- lists-conditionals-assignment/src/index.js
+++ lists-conditionals-assignment/src/index.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDom.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
