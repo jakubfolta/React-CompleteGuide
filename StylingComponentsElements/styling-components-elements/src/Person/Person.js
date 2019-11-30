e0e5bec05f0eabedaf852b1ff092446379fc5a70
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-30 18:26:27 +0000

    Continue React course - Styled components.

--- StylingComponentsElements/styling-components-elements/src/Person/Person.js
+++ StylingComponentsElements/styling-components-elements/src/Person/Person.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Person.css'
+// import './Person.css'
+
+const StyledDiv = styled.div`
+  width: 80%;
+  margin: 10px auto;
+  box-shadow: 5px 5px 10px;
+  border: 2px solid hsl(100, 43%, 50%);
+  padding: 16px;
+  text-align: center;
+`
 
 const person = (props) => {
   const style = {
@@ -13,11 +23,11 @@ const person = (props) => {
   };
 
   return (
-    <div className="Person" style={style}>
+    <StyledDiv style={style}>
       <p onClick={props.click}>I'm a {props.name}. And I'm a it {props.age} years old.</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    </StyledDiv>
   )
 };
 
