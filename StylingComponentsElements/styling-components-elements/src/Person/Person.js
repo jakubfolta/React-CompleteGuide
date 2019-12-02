25f3b962cd5aaeff5a0a8e767351f3f84907bd43
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-12-02 23:09:04 +0000

    Continue course - CSS modules media queries.

--- StylingComponentsElements/styling-components-elements/src/Person/Person.js
+++ StylingComponentsElements/styling-components-elements/src/Person/Person.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 
-// import './Person.css'
-
-const StyledDiv = styled.div`
-  width: 80%;
-  margin: 10px auto;
-  box-shadow: 5px 5px 10px;
-  border: 2px solid hsl(100, 43%, 50%);
-  padding: 16px;
-  text-align: center;
-
-  @media (min-width: 500px) {
-    width: 30%;
-  }
-`;
+import classes from './Person.css';
 
 const person = (props) => {
   return (
-    <StyledDiv>
+    <div>
       <p onClick={props.click}>I'm a {props.name}. And I'm a it {props.age} years old.</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </StyledDiv>
+    </div>
   )
 };
 
