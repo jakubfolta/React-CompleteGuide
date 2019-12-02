f632da220b355f41e1af161a8d80646f16d545e5
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-12-02 23:21:58 +0000

    Continue course - CSS modules media queries.

--- StylingComponentsElements/styling-components-elements/src/Person/Person.js
+++ StylingComponentsElements/styling-components-elements/src/Person/Person.js
@@ -4,7 +4,7 @@ import classes from './Person.css';
 
 const person = (props) => {
   return (
-    <div>
+    <div className={classes.Person} >
       <p onClick={props.click}>I'm a {props.name}. And I'm a it {props.age} years old.</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
