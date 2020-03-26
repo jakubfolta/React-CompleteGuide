5b06de609d2bf0c3124edcbd25c3d44d309c75ab
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-26 11:11:33 +0000

    Solidifying basic knowledge

--- basefeatures-assignment/src/index.js
+++ basefeatures-assignment/src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
 
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-//
-// ReactDOM.render(<App />, document.getElementById('root'));
-// serviceWorker.unregister();
