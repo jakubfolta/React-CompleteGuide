3c6b8a5486d0dfad68b655a83d500b9ce16c76d6
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-26 11:39:21 +0000

    Solidifying basic knowledge

--- basefeatures-assignment/src/UserInput/UserInput.js
+++ basefeatures-assignment/src/UserInput/UserInput.js
@@ -3,10 +3,39 @@ import React from 'react';
 const userInput = props => {
   return (
     <div>
-      <input type="text" onChange={props.nameChange} value={props.userName}/>
+      <input type="text"></input>
     </div>
   )
-};
-
+}
 
 export default userInput;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+//
+// const userInput = props => {
+//   return (
+//     <div>
+//       <input type="text" onChange={props.nameChange} value={props.userName}/>
+//     </div>
+//   )
+// };
+//
+//
+// export default userInput;
--- basefeatures-assignment/src/UserOutput/UserOutput.js
+++ basefeatures-assignment/src/UserOutput/UserOutput.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-import './Output.css';
-
 const userOutput = props => {
   return (
-    <div class='Output'>
-      <p>My name is {props.userName}.</p>
+    <div>
+      <p>My name is {props.name}</p>
     </div>
   )
 }
 
-
 export default userOutput;
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+//
+// import './Output.css';
+//
+// const userOutput = props => {
+//   return (
+//     <div class='Output'>
+//       <p>My name is {props.userName}.</p>
+//     </div>
+//   )
+// }
+//
+//
+// export default userOutput;
