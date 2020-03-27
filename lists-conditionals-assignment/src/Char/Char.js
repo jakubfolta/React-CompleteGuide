038df82ac403dbc76136ae5f8eddadb525b21b46
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-27 11:50:15 +0000

    Solidifying knowledge - Lists and Conditionals assignment

--- lists-conditionals-assignment/src/Char/Char.js
+++ lists-conditionals-assignment/src/Char/Char.js
@@ -1,5 +1,16 @@
+import React from 'react';
 
+import './Char.css';
 
+const char = props => {
+  return (
+    <div className="Char" onClick={props.delete}>
+      <p></p>
+    </div>
+  )
+}
+
+export default char;
 
 
 
