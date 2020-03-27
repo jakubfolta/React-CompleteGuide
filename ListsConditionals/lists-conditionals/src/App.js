4d7143af7a3c174303dd73577cfde0f97d80088a
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-27 09:41:28 +0000

    Solidifying knowledge - Lists and Conditionals

--- ListsConditionals/lists-conditionals/src/App.js
+++ ListsConditionals/lists-conditionals/src/App.js
@@ -13,7 +13,19 @@ class App extends Component {
     showPersons: false
   }
 
+  nameChangedHandler = (e, id) => {
+    const personIndex = this.state.persons.findIndex(el => el.id === id);
 
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = e.target.value;
+
+    const updatedPersons = [...this.state.persons];
+    updatedPersons[personIndex] = person;
+
+    this.setState({persons: updatedPersons})
+  }
 
 
 
