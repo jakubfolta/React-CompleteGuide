ba660123035f025805bc2391aa1451e457451156
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-28 11:23:00 +0000

    Solidifying knowledge - Styling components and elements

--- StylingComponentsElements/styling-components-elements/src/App.js
+++ StylingComponentsElements/styling-components-elements/src/App.js
@@ -62,14 +62,7 @@ class App extends Component {
       btnClasses = classes.Red;
     }
 
-    const assignClasses = [];
-
-    if (this.state.persons.length <= 2 && this.state.persons.length > 0) {
-      assignClasses.push(classes.Red);
-    }
-    if (this.state.persons.length === 1) {
-      assignClasses.push(classes.Bold);
-    }
+    
 
     return (
       <div className={classes.App}>
