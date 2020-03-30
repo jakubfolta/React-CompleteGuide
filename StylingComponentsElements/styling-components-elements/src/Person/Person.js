095c893c49efb261e513d571e4be05f3046e276c
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-30 10:34:50 +0100

    Solidifying knowledge - Styling components and elements - Styled components

--- StylingComponentsElements/styling-components-elements/src/Person/Person.js
+++ StylingComponentsElements/styling-components-elements/src/Person/Person.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
-import classes from './Person.css';
+// import classes from './Person.css';
+
+const StyledDiv = styled.div`
+  background-color: hsl(129, 100%, 50%);
+  width: 80%;
+  margin: 10px auto;
+  box-shadow: 3px 3px 10px;
+  border: 2px solid hsl(180, 23%, 50%);
+  padding: 16px;
+  text-align: center;
+  overflow: hidden;
+
+  @media (min-width: 500px) {
+    width: 30%;
+    box-shadow: 0 9px 0 -6px hsl(0, 0%, 0%);
+  }
+`
 
 const person = (props) => {
+
   return (
-    <div className={classes.Person} >
+    <StyledDiv>
       <p onClick={props.click}>I'm a {props.name}. And I'm a it {props.age} years old.</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    </StyledDiv>
   )
 };
 
