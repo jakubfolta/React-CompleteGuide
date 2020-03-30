64f4bb0916f0adf3536479d94690f8ce5a70bea9
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-30 14:49:45 +0100

    Deeper into components - proper structure

--- DeeperIntoComponents/deeper-into-components/src/components/Cockpit/Cockpit.js
+++ DeeperIntoComponents/deeper-into-components/src/components/Cockpit/Cockpit.js
@@ -1,9 +1,10 @@
 import React from 'react';
+
 import classes from './Cockpit.module.css';
 
-const cockpit = props => {
+const Cockpit = props => {
+
   const assignClasses = [];
-  let btnClasses = '';
 
   if (props.persons.length <= 2 && props.persons.length > 0) {
     assignClasses.push(classes.Red);
@@ -11,17 +12,16 @@ const cockpit = props => {
   if (props.persons.length === 1) {
     assignClasses.push(classes.Bold);
   }
-  if (props.showPersons) {
-    btnClasses = classes.Red;
-  }
 
   return (
     <div className={classes.Cockpit}>
       <h1>Hi, I'm React app.</h1>
       <p className={assignClasses.join(' ')}>This is working.</p>
-      <button className={btnClasses} onClick={props.toggle}>Show persons!</button>
+      <button
+        onClick={props.toggle}>Show persons! </button>
     </div>
   )
-};
+}
 
-export default cockpit;
+
+export default Cockpit;
