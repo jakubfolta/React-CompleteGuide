251b1277e58db70ec88a04d655897e0c955e9fb8
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-31 09:54:44 +0100

    Deeper into components - Component lifecycle

--- DeeperIntoComponents/deeper-into-components/src/containers/App.js
+++ DeeperIntoComponents/deeper-into-components/src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor')
+  }
+
   state = {
     persons: [
       { id: 'dsa', name: 'Max', age: 32 },
@@ -12,6 +17,15 @@ class App extends Component {
     ],
 
     showPersons: false
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
   }
 
   nameChangedHandler = (event, id) => {
@@ -43,6 +57,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
