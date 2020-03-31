fce64532ab6047fe2b068f6ee1797ac2224b49f4
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-31 10:25:15 +0100

    Deeper into components - Update component lifecycle

--- DeeperIntoComponents/deeper-into-components/src/containers/App.js
+++ DeeperIntoComponents/deeper-into-components/src/containers/App.js
@@ -28,6 +28,10 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
