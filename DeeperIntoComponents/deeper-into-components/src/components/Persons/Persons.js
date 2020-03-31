646734411a3837bafec6b007ade6e32285ebc211
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-31 10:22:19 +0100

    Deeper into components - Update component lifecycle

--- DeeperIntoComponents/deeper-into-components/src/components/Persons/Persons.js
+++ DeeperIntoComponents/deeper-into-components/src/components/Persons/Persons.js
@@ -3,14 +3,23 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
-  static getDerivedStateFromProps(props, state) {
-    console.log('[Persons.js] getDerivedStateFromProps');
-    return state;
-  }
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
 
   shouldComponentUpdate(nextProps, nextState) {
     console.log('[Persons.js] shouldComponentUpdate');
     return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot!'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate', snapshot.message);
   }
 
   render() {
