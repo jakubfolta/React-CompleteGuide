1b6e75a0f82ea8c9778cd03705af5708732e2a86
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-03-31 09:58:08 +0100

    Deeper into components - Update component lifecycle

--- DeeperIntoComponents/deeper-into-components/src/components/Persons/Person/Person.js
+++ DeeperIntoComponents/deeper-into-components/src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.module.css';
 
-const person = (props) => {
-  console.log('[Person.js] rendering')
-  return (
-    <div className={classes.Person} >
-      <p onClick={props.click}>I'm a {props.name}. And I'm a it {props.age} years old.</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  )
-};
+class Person extends Component {
+  render() {
+    console.log('[Person.js] rendering')
+    return (
+      <div className={classes.Person} >
+        <p onClick={this.props.click}>I'm a {this.props.name}. And I'm a it {this.props.age} years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
