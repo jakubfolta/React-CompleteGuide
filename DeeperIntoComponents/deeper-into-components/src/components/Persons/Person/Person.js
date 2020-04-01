4a64b8a885c455076138ac882cec3e8d137228a3
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-01 09:11:22 +0100

    Deeper into components - rendering adjacent JSX elements (Fragment)

--- DeeperIntoComponents/deeper-into-components/src/components/Persons/Person/Person.js
+++ DeeperIntoComponents/deeper-into-components/src/components/Persons/Person/Person.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component, Fragment } from 'react';
+// import Aux from '../../../hoc/Auxiliary';
 
-import classes from './Person.module.css';
+// import classes from './Person.module.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering')
     return (
-      <Aux >
+      <Fragment>
         <p onClick={this.props.click}>I'm a {this.props.name}. And I'm a it {this.props.age} years old.</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name}/>
-      </Aux>
+      </Fragment>
     );
   }
 }
