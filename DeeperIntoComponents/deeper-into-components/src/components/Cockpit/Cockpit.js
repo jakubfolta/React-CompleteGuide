a253a7e84b773013e510cd58e8bb56039c5ede32
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-02 10:13:35 +0100

    Deeper into components - Refs with React Hooks

--- DeeperIntoComponents/deeper-into-components/src/components/Cockpit/Cockpit.js
+++ DeeperIntoComponents/deeper-into-components/src/components/Cockpit/Cockpit.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.module.css';
 
 const Cockpit = props => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     // Http request...
-    const timer = setTimeout(() => {
-      alert('Saved data to cloud.')
-    }, 1000)
+    // const timer = setTimeout(() => {
+    //   alert('Saved data to cloud.')
+    // }, 1000)
+    toggleBtnRef.current.click();
     return () => {
-      clearTimeout(timer);
-      console.log('[Cockpit.js] cleanup work in useEffect');
+      // clearTimeout(timer);
+      // console.log('[Cockpit.js] cleanup work in useEffect');
     };
   }, []); // [props.persons]
 
@@ -41,6 +44,7 @@ const Cockpit = props => {
       <h1>{props.title}</h1>
       <p className={assignClasses.join(' ')}>This is working.</p>
       <button
+        ref={toggleBtnRef}
         className={btnClass}
         onClick={props.toggle}>Show persons! </button>
     </div>
