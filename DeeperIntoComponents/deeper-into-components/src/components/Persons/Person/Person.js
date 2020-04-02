2b3c1f5443160a2b267c5e7c5dcc614dc08c7fa3
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-02 10:01:20 +0100

    Deeper into components - Refs

--- DeeperIntoComponents/deeper-into-components/src/components/Persons/Person/Person.js
+++ DeeperIntoComponents/deeper-into-components/src/components/Persons/Person/Person.js
@@ -5,13 +5,27 @@ import withClass from '../../../hoc/withClass'
 import classes from './Person.module.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props)
+      this.inputElRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.inputElRef.current.focus();
+  }
+
   render() {
     console.log('[Person.js] rendering')
     return (
       <Fragment>
         <p onClick={this.props.click}>I'm a {this.props.name}. And I'm {this.props.age} years old.</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          // ref={inp => {this.inpElement = inp}}
+          ref={this.inputElRef}
+          value={this.props.name}/>
       </Fragment>
     );
   }
