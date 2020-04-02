51814464e79f93b3ed88fc780d45f1276dedfe72
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-02 06:47:27 +0100

    Deeper into components - setting state correctly

--- DeeperIntoComponents/deeper-into-components/src/containers/App.js
+++ DeeperIntoComponents/deeper-into-components/src/containers/App.js
@@ -55,7 +55,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    
+    this.setState((prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
+    })
   }
 
   deletePersonHandler = personIndex => {
