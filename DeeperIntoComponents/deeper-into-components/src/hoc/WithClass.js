0af7cad1f9165d8d5d1a7b69c8dad42eaa112d90
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-02 08:59:17 +0100

    Deeper into components - setting state correctly

--- DeeperIntoComponents/deeper-into-components/src/hoc/WithClass.js
+++ DeeperIntoComponents/deeper-into-components/src/hoc/WithClass.js
@@ -1,19 +1,8 @@
-import React from 'react';
-
-const withClass = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent {...props}/>
-    </div>
-  );
-};
-
-export default withClass;
-
+import
 // import React from 'react';
 //
 // const WithClass = props => (
-//   <div className={props.classes}>props.children</div>
+//   <div className={props.classes}>{props.children}</div>
 // )
 //
 // export default WithClass;
