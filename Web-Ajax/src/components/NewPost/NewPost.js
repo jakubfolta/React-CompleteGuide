cced91098baf1734f2d0bf3c03b1cf1d5ae8606a
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-14 08:50:44 +0100

    Posting data to server - Ajax in React

--- Web-Ajax/src/components/NewPost/NewPost.js
+++ Web-Ajax/src/components/NewPost/NewPost.js
@@ -11,7 +11,13 @@ class NewPost extends Component {
     }
 
     postDataHandler = () => {
+        const post = {
+          title: this.state.title,
+          content: this.state.content,
+          author: this.state.author
+        }
 
+        axios.post('https://jsonplaceholder.typicode.com/posts', post);
     }
 
     render () {
@@ -19,7 +25,8 @@ class NewPost extends Component {
             <div className="NewPost">
                 <h1>Add a Post</h1>
                 <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <input type="text" value={this.state.title} onChange={e => this.setState({title: e.target.value})}></input>
+
                 <label>Content</label>
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
