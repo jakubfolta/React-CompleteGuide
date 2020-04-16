bff7f9e739b76010dc8bbab17c80bc1ff55986f6
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-16 10:11:50 +0100

    Set up and render routes

--- Web-Ajax/src/containers/Blog/Blog.js
+++ Web-Ajax/src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -15,7 +16,8 @@ class Blog extends Component {
                   </ul>
                 </nav>
               </header>
-              <Posts />
+              <Route path="/" render={() => <Posts />} />
+              <Route path="/new-post" render={() => <Posts />} />
             </div>
         );
     }
