54c68ab786ef6129f2674c47fe59f91e5868ddc2
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-19 19:19:14 +0100

    Extracting Route parameters

--- Web-Ajax/src/containers/Blog/Blog.js
+++ Web-Ajax/src/containers/Blog/Blog.js
@@ -14,13 +14,7 @@ class Blog extends Component {
               <header className="Blog">
                 <nav>
                   <ul>
-                    <li><NavLink
-                      exact
-                      to='/'
-                      activeClassName="active"
-                      activeStyle={{
-                        color: 'red'
-                      }} >Home</NavLink></li>
+                    
                     <li><NavLink
                       to={{
                         // pathname: this.props.match.url + 'new-post',
