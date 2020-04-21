528d3e5fe38575c5138f494799c1b3e67d8c144e
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-21 09:36:06 +0100

    Add lazy loading

--- Web-Ajax/src/App.js
+++ Web-Ajax/src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/my-app"> // "/" default
       <BrowserRouter>
         <div className="App">
           <Blog />
