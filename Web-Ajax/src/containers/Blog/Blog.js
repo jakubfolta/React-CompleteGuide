0640f608de05c566885515c6c856dbdca8d2d837
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-21 09:21:26 +0100

    Add lazy loading

--- Web-Ajax/src/containers/Blog/Blog.js
+++ Web-Ajax/src/containers/Blog/Blog.js
@@ -3,11 +3,16 @@ import { Route, NavLink, withRouter, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
     state = {
-      auth: false
+      auth: true
     }
 
     render () {
@@ -36,7 +41,7 @@ class Blog extends Component {
                 </nav>
               </header>
               <Switch>
-                {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                 <Route path="/posts" component={Posts} />
                 <Route render={() => <h1>Not found</h1>} />
                 {/* <Redirect from="/" to="/posts" /> */}
