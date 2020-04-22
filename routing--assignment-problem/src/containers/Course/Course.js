ecb68d8ce04846cefb903636e76dd6cb82be670a
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-04-22 09:39:44 +0100

    Repeat assignment - solidifying knowledge

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -3,10 +3,11 @@ import { withRouter } from 'react-router-dom';
 
 class Course extends Component {
     render () {
+      console.log(this.props);
         return (
             <div>
                 <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <p>You selected the Course with ID: {this.props.match.params.id}</p>
             </div>
         );
     }
