21ef24b4c8dee8bf3ffc1de958eb446aec007081
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-01 10:09:46 +0100

    Start Redux

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
