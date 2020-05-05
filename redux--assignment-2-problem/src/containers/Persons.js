14ba954b7db41516e9571c8b97eb539d71cfc94c
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-05 13:35:06 +0100

    Redux - assignment - implement Redux - solidifying

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -3,43 +3,36 @@ import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
+import * as actionTypes from './../store/actions';
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
-
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
-
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
-
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
+                <AddPerson personAdded={this.props.onAddPersonHandler} />
+                {this.props.persons.map(person => (
                     <Person
                         key={person.id}
                         name={person.name}
                         age={person.age}
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        clicked={() => this.props.onDeletePersonHandler(person.id)}/>
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+  return {
+    persons: state.persons
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onAddPersonHandler: () => dispatch({type: actionTypes.ADD_PERSON}),
+    onDeletePersonHandler: (id) => dispatch({type: actionTypes.ADD_PERSON, id: id})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- redux--assignment-2-problem/src/store/reducer.js
+++ redux--assignment-2-problem/src/store/reducer.js
@@ -7,12 +7,19 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_PERSON:
+      const newPerson = {
+          id: Math.random(), // not really unique but good enough here!
+          name: 'Q',
+          age: Math.floor( Math.random() * 40 )
+      }
       return {
-
+        ...state,
+        persons: state.persons.concat(newPerson)
       }
     case actionTypes.DELETE_PERSON:
       return {
-
+        ...state,
+        persons: state.persons.filter(person => person.id !== action.id)
       }
   }
   return state
