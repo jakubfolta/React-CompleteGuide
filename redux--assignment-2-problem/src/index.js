a757c518f2d21c207ade2d3f4c7b17a7ef50c232
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-05 13:08:05 +0100

    Redux - assignment - implement Redux - solidifying - create store

--- redux--assignment-2-problem/src/index.js
+++ redux--assignment-2-problem/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
