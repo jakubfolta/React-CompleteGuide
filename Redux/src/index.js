2e4e819938932e1f4f219880f520f8f39af84f35
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-11 10:43:15 +0100

    Implement middleware

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './App';
@@ -25,7 +25,7 @@ const logger = store => {
   }
 }
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
