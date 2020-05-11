67acd469dff2913ad22f2ae722ac4e2d97c3644d
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-11 14:08:42 +0100

    Restructure actionCreators

--- Redux/src/store/actions/result.js
+++ Redux/src/store/actions/result.js
@@ -1,16 +1,18 @@
 import * as actionTypes from './actionTypes';
 
 const storeResult = (result) => {
-  const updatedResult = result * 2;
+  // const updatedResult = result * 2;
   return {
     type: actionTypes.STORE,
-    result: updatedResult
+    result: result
   }
 }
 
 export const store = (result) => {
-  return dispatch => {
+  return (dispatch, getState) => {
     setTimeout(() => {
+      const oldCounter = getState().ctr.counter;
+      console.log(oldCounter);
       dispatch(storeResult(result))
     }, 2000)
   }
