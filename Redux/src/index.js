ec616b5f7d8f72e45aee00c50bbc9db2e71aad7d
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-11 10:48:23 +0100

    Implement middleware - solidifying

--- Redux/src/index.js
+++ Redux/src/index.js
@@ -14,7 +14,16 @@ const rootReducer = combineReducers({
   res: resultReducer
 });
 
-
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
+    }
+  }
+}
 
 
 // const logger = store => {
