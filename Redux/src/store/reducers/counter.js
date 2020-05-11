c9e53ff5f868db8418814bc651b54f1d57fd8f7e
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-11 11:25:46 +0100

    Implement middleware

--- Redux/src/store/reducers/counter.js
+++ Redux/src/store/reducers/counter.js
@@ -26,8 +26,9 @@ const reducer = (state = initialState, action) => {
         ...state,
         counter: state.counter - action.payload.value
       }
+    default:
+      return state;
   }
-  return state;
 };
 
 export default reducer;
--- Redux/src/store/reducers/result.js
+++ Redux/src/store/reducers/result.js
@@ -7,23 +7,24 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE:
-    return {
-      ...state,
-      results: state.results.concat({id: new Date(), value: action.result})
-    }
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: action.result})
+      }
     case actionTypes.DELETE:
     // const id = 2;
     // const index = 3;
     // const newArray = [...state.results];
     // newArray.splice(index, 1);
 
-    const newArray = state.results.filter(el => el.id !== action.elId);
-    return {
-      ...state,
-      results: newArray
-    }
+      const newArray = state.results.filter(el => el.id !== action.elId);
+      return {
+        ...state,
+        results: newArray
+      }
+    default:
+      return state;
   }
-  return state;
 };
 
 export default reducer;
