934001053aa42afaa18b61f5df7a51ef7b38e42a
Jakub Folta <jakubfolta@yahoo.co.uk>
2020-05-12 08:50:28 +0100

    Use utility function - solidifying

--- Redux/src/store/utility.js
+++ Redux/src/store/utility.js
@@ -1,6 +1,9 @@
-export const updateObject = (oldObject, updatedValues) => {
-  return {
-    ...oldObject,
-    ...updatedValues
-  }
-}
+
+
+
+// export const updateObject = (oldObject, updatedValues) => {
+//   return {
+//     ...oldObject,
+//     ...updatedValues
+//   }
+// }
